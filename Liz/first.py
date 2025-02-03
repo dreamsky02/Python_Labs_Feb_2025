bf692250b6127b66805f906a2118c3a2055a9dba
Elizabeth Mares <garcia.elizabeth21@gmail.com>
2025-02-03 22:53:40 +0000

    Added first.py

---
+++ Liz/first.py
@@ -0,0 +1,17 @@
+#!./usr/bin/env python3
+#Author: DCameron
+#Version: 1.0
+#Description: This is our First script
+
+name= "Elizabeth Mares"
+
+print ("My name is", name)
+print  ("My name is" + name)
+
+import random
+print ("Lucky number is", randint (1,50))
+
+import math
+print ("Cosine of 0.5 is", cos (0.5))
+
+       
