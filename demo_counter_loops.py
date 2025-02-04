d2a3499baeefceeab9794561f63b923578fec460
Elizabeth Mares <garcia.elizabeth21@gmail.com>
2025-02-04 20:20:52 +0000

    Added demo_counter_loops.py

---
+++ demo_counter_loops.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env Python 3
+# Author: EMares
+# Description: This script will demo
+#
+#
+"""
+
+Docsctring:
+"""
+
+count =0#. 1. Intialize counter
+while count <10:
+    print(count)
+    count +=1
+
+for num in range(0,11,2):
+    print(num)
+for num in range(10):
+    print(num)
