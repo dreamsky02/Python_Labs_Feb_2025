b9260667368af7bda124bcaaea1399d714bbe136
Elizabeth Mares <garcia.elizabeth21@gmail.com>
2025-02-04 20:37:24 +0000

    Added demo_iterator_loop.py.py

---
+++ demo_iterator_loop.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env Python 3
+# Author: EMares
+# Description: This script will demo
+#
+#
+"""
+
+Docsctring:
+
+
+"""
+films = ['casshern', 'pulp fiction', 'matrix', 'pursuit of happiness',
+         'titanic, lotr','topgun']
+for film in films:
+    print(film, end="\n\n")
+
+idx = 0
+for film in enumerate(films):
+    print(film.upper(). end="\n)
+    films[id] = film.upper()
+    idx +=1
+print(films)
+
+
+for (idx, film) in enumerate(films, start=0):
+    print(film)
