6b9650fb428e4e9f0f4e086637674fc5633374ce
Elizabeth Mares <garcia.elizabeth21@gmail.com>
2025-02-05 01:06:32 +0000

    Added demo_dict_movies.py

---
+++ demo_dict_movies.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env Python 3
+# Author: EMares
+# Description: This script will demo """Docsctring:"""
+
+movies = { 'william': ['fury', 'pulp fiction', 'shawshank redemption'],
+           'keril': ['pulp fiction','the departed', 'snatch'
+           'mark': ['fith element','armagedon','slapshot']}
