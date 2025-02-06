b2411426fa4711cb6591093b5391aa47a978dcfb
Admin <garcia.elizabeth21@gmail.com>
2025-02-06 19:04:08 +0000

    Added div.py

---
+++ calc/div.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env Python 3
+# Author: EMares
+# Description: This script will demo
+#
+#
+"""
+
+Docsctring:Advanced calc Funtions
+
+"""
+import sys
+
+def mod(x, z):
+    return x % z
+def power(x, z):
+    return x** z
+def sqrt(x):
+    return round(x**0.5, 2)
+
+print("Advanced Calculator App")
+print("_" * 30)
+
+
+print(f"100 / 30 = {mod(100, 30)}")
+print(f"100 ** 3 = {power(100, 3)}")
+print(f"\N{square root}100 = {sqrt(100)}")
+
+
+
+
+
+
+
+
+sys.exit(0)
