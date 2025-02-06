3d614b5d1ba9de09b328932924b2c8b968469876
Admin <garcia.elizabeth21@gmail.com>
2025-02-06 17:34:14 +0000

    Added exercise_9.py

---
+++ exercise_9.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env Python 3
+# Author: EMares
+# Description: This script will demo
+#
+#
+"""
+
+Docsctring:
+
+"""
+def fange(start, stop, step=0.25):
+    curr =float(start)
+    while curr<stop:
+     yield curr
+     curr +-step
+
