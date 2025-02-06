9a14d0c54d8c230eba19214887708e04e99e1f9e
Admin <garcia.elizabeth21@gmail.com>
2025-02-06 00:46:30 +0000

    Added demo_userFuncion.py

---
+++ demo_userFuncion.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env Python 3
+# Author: EMares
+# Description: This script will demo
+#
+#
+"""
+
+Docsctring:
+
+"""
+
+def say_hello(greeting, recipient):
+    message = f"{greeting} {recipient}"
+    print(message)
+    return None
+#cmds
+
+
+
+say_hello(greeting="hola",recipient= "amigos")
+say_hello(greeting="ciao", recipient="amici")
+say_hello(greeting="nmaste", recipient="dosto")
+say_hello(greeting="yo", recipient="homies")
+say_hello("bonjour", "mes amis")
+say_hello("bonjour", 5)
